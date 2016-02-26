74c245270c8c7d4c6c16133ffd02ec0516969d0b
dphaener <dphaener@gmail.com>
2016-02-26 09:31:41 -0800

    Initial commit

---
+++ example/react-reading-time.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ReadingTime from '../src/reading-time'
+
+class ReactReadingTime extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    };
+  }
+
+  updateText(ev) {
+    this.setState({ text: ev.target.value });
+  }
+
+  render() {
+    return (
+      <div className='container' style={{ marginTop: '50px' }}>
+        <div className='col-lg-8 col-lg-offset-2 form-group'>
+          <textarea
+            value={this.state.text}
+            onChange={::this.updateText}
+            className='form-control'
+            style={{ height: '500px', resize: 'none' }}>
+          </textarea>
+        </div>
+        <ReadingTime text={this.state.text} className='col-lg-2 well' />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<ReactReadingTime />, document.getElementById('react'))
---
+++ src/reading-time.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+export default class ReadingTime extends React.Component {
+  static propTypes = {
+    wordsPerMinute: React.PropTypes.number,
+    textColor: React.PropTypes.string
+  };
+
+  static defaultProps = {
+    wordsPerMinute: 270,
+    textColor: 'blue'
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      readTime: 0
+    }
+  }
+
+  componentWillMount() {
+    this.updateReadingTime(this.props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.updateReadingTime(nextProps);
+  }
+
+  updateReadingTime(props) {
+    const words = this.countWords(props.text);
+    const readTime = Math.round(words / props.wordsPerMinute);
+
+    this.setState({ readTime });
+  }
+
+  countWords(text) {
+    return text.split(/\s+/).length
+  }
+
+  render() {
+    const { textColor, ...rest } = this.props;
+    const { readTime } = this.state;
+    const minutes = readTime === 1 ? 'minute' : 'minutes';
+
+    return (
+      <div {...rest}>
+        <p style={{ color: textColor }}>
+          Estimated read time:<br /><br />
+          <span>{readTime} {minutes}</span>
+        </p>
+      </div>
+    )
+  }
+}
